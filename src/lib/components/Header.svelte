fca283bda0086f1137016869515de3c6da7b1081
kairrro <damnkyro@proton.me>
2024-04-25 11:11:02 +0100

    Added NotFound route

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,8 +2,8 @@
     <a href="/" class="">Logo here</a>
 
     <div class="flex items-center gap-12">
-        <a href="/">Home</a>
-        <a href="/">FAQ</a>
-        <a href="/">Placeholder</a>
+        <a href="/" class="hover:underline underline-offset-4 transition" >Home</a>
+        <a href="/faq" class="hover:underline underline-offset-4 transition">FAQ</a>
+        <a href="/" class="hover:underline underline-offset-4 transition">Placeholder</a>
     </div>
 </div>
