df847282ca64dc14f35441355b2ea1b559e3cd3c
kairrro <damnkyro@proton.me>
2024-04-25 10:55:22 +0100

    added not found route

---
+++ src/lib/components/NotFound.svelte
@@ -0,0 +1,10 @@
+<div class="flex flex-col items-center mt-32 text-[#f1f1f1]">
+    <div class="flex flex-col items-center gap-4">
+        <h1 class="text-8xl font-semibold" >404</h1>
+        <p>Page not found!</p>
+    </div>
+
+    <a href="/" class="mt-10 border-[#f1f1f1] px-4 py-2 rounded-lg border-[1px] hover:bg-[#f1f1f1] hover:text-black hover:font-medium transition">
+        Return to homepage
+    </a>
+</div>
