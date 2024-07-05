2eaca798bf03acdf3e4c2e20135049209eb05a6a
kairrro <damnkyro@proton.me>
2024-07-05 17:14:53 +0100

    updated not found

--- src/lib/components/NotFound.svelte
+++ src/lib/components/NotFound.svelte
@@ -1,10 +1,15 @@
+<script>
+
+  import { buttonBackground, descriptionColor } from "$lib/scripts/stores/appearance";
+
+</script>
 <div class="flex flex-col items-center mt-32 text-[#f1f1f1]">
     <div class="flex flex-col items-center gap-4">
         <h1 class="text-8xl font-semibold" >404</h1>
-        <p>Page not found!</p>
+        <p class="{$descriptionColor} text-sm" >Looks like this page does not exist...</p>
     </div>
 
-    <a href="/" class="mt-10 border-[#f1f1f1] px-4 py-2 rounded-lg border-[1px] hover:bg-[#f1f1f1] hover:text-black hover:font-medium transition">
-        Return to homepage
+    <a href="/" class="mt-10 border-[#f1f1f1] rounded-full text-sm py-2 px-6 {$buttonBackground} hover:font-medium transition">
+        Return to home
     </a>
 </div>
