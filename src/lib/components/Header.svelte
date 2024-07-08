fdd4245837854790924f62b29d76c6e7cafcf4e7
kairrro <damnkyro@proton.me>
2024-07-08 22:32:38 +0100

    updated sidebar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -49,50 +49,21 @@
             <div class="flex flex-col px-4 text-sm" >
                 <a
                     href="/"  
-                    class="flex items-center gap-2 w-full py-2"  
+                    class="w-full py-2"  
                     on:click={closeDropdown}
                 >
-                    {@html Profile(undefined, "22px")}
                     <p>Home</p>
                 </a>
                 <a
                     href="/"  
-                    class="flex items-center gap-2 w-full py-2"  
+                    class="w-full py-2"  
                     on:click={closeDropdown}
                 >
-                    {@html Star("#f1f1f1", "22px")}
                     <p>Placeholder</p>
                 </a>
             </div>
         </div>
-
-        <div class="flex flex-col gap-4 text-sm" >
-            <div class="border-[1px] border-zinc-700 w-full" ></div>
-
-            <div class="flex flex-col px-4" >
-                <a
-                    href="/"    
-                    class="flex items-center gap-2 w-full py-2" 
-                    on:click={closeDropdown}
-                >
-
-                    {@html Settings("#818097", "22px")}
-                    <p>Settings</p>
-                </a>
-
-                <a
-                    href="/"    
-                    class="flex items-center gap-2 w-full py-2"  
-                    on:click={closeDropdown}
-                >
-                    {@html Info(undefined, "22px")}
-                    <p>Help</p>
-                </a>
-            </div>
-        </div>
     </div>
 
-    <div class="hidden md:flex fixed bg-black opacity-50 h-screen w-screen z-10" >
-
-    </div>
+    <div class="hidden md:flex fixed bg-black opacity-50 h-screen w-screen z-10" ></div>
 {/if}
